79f93b24f91f8ed4d960acf3efa65b286fc45a67
Klee Collage <klee.collage@gmail.com>
2025-01-08 21:07:19 -0600

    ForEach

---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+        int[] ages = {5, 40, 50, 26};
+        for(int age : ages){
+            System.out.println("Age: " + age);
+        }
+    }
+}

a80b6a3b3fa17dad7c54ad16b22e35b077f7dc42
Klee Collage <klee.collage@gmail.com>
2025-01-09 18:46:54 -0600

    First commit

---
+++ src/main/java/gm/fit_zone/FitZoneApplication.java
@@ -0,0 +1,13 @@
+package gm.fit_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gm/fit_zone/FitZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package gm.fit_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
